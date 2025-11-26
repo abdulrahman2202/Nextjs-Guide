c958ea548bb6bdbd57fca5c899313b6579182546
abdul@gmail.com <abdulrahman2.ar44@gmail.com>
2025-11-26 20:14:27 +0530

    next js setup done

---
+++ app/useState/page.tsx
@@ -0,0 +1,15 @@
+const useStateDemo = ()=>{
+
+    let name = "Abdul Rahman"
+
+    name = "hero"
+    return(
+        <div>
+            <p>
+                hello from useStateDemo {name}
+            </p>
+        </div>
+    )
+}
+
+export default useStateDemo;
